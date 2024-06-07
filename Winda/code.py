7790baad87f539e05f1c9abca13dac5ff0f3c988
tsmyda <tomasz.smyda@gmail.com>
2024-06-07 13:12:15 +0200

    update

---
+++ Winda/code.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import logicmin
+df = pd.read_csv('truthtable.txt', header=None, names=['data'])
+
+df["input"] = df["data"].str[:7]
+df["output"] = df["data"].str[7:]
+
+input_list = df["input"].values.tolist()
+output_list = df["output"].values.tolist()
+
+minimalizer = logicmin.TT(7, 7)
+
+for i in range(len(input_list)):
+    minimalizer.add(input_list[i], output_list[i])
+    
+solution = minimalizer.solve()
+
+print(solution.printN(xnames = ["F1","F2","D1","D2","B0","B1","B2"],
+                      ynames = ["F1'","F2'","D1'","D2'","B0'","B1'","B2'"]))
